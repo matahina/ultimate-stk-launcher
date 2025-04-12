2932251eb424a48fe40748b12888e73f8200d998
matahina <matahina@posteo.net>
2025-04-12 23:13:39 +0200

    Patches for "stk speed"

--- libs/recipes/install_stk_speed.sh
+++ libs/recipes/install_stk_speed.sh
@@ -5,9 +5,9 @@ git clone https://github.com/supertuxkart/stk-code stk-code-speed
 svn co https://svn.code.sf.net/p/supertuxkart/code/stk-assets stk-assets
 cd stk-code-speed
 git checkout 42d4eaa
-wget https://github.com/matahina/Miscellanous-STK-files/raw/main/stk_speed_anonymouse_coder/src.zip
-unzip -o src.zip
+wget https://raw.githubusercontent.com/matahina/Miscellanous-STK-files/refs/heads/main/stk_speed_anonymouse_coder/0001-the_patch_for_stk_speed.patch
 wget https://raw.githubusercontent.com/matahina/Miscellanous-STK-files/refs/heads/main/stk_speed_anonymouse_coder/0001-patcherella.patch
+git apply 0001-the_patch_for_stk_speed.patch
 git apply 0001-patcherella.patch
 mkdir cmake_build
 cd cmake_build
